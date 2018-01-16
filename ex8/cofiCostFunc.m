af51a73b2aac1910c05829181a89bfc9f89b9256
Kou Guandong <gdkou90@gmail.com>
2018-01-16 21:17:47 +0800

    collaborative filtering function implemented with regulated paramters

--- ex8/cofiCostFunc.m
+++ ex8/cofiCostFunc.m
@@ -42,7 +42,14 @@ Theta_grad = zeros(size(Theta));
 
 
 
+J_rated = (X * Theta' - Y).^2;
 
+
+
+J = sum(sum(J_rated(R == 1)))/2 + lambda/2 .* sum(sum(Theta.^2)) + lambda/2 .* sum(sum(X.^2));
+
+X_grad = ((X * Theta' - Y) .* R) * Theta + lambda .* X;
+Theta_grad = ((X * Theta' - Y) .* R)' * X + lambda .* Theta;
 
 
 
